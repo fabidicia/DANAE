a739e99ad94661802f71c1edbc6f4e7d08dd2bfb
fabidicia <62762235+fabidicia@users.noreply.github.com>
2020-04-22 20:08:22 +0200

    Update networks.py

--- networks.py
+++ networks.py
@@ -14,7 +14,7 @@ from itertools import chain
 
 
 class MyLSTM(nn.Module):
-    def __init__(self,device = torch.device("cpu"),n_inputs=39):
+    def __init__(self, device=torch.device("cpu"), n_inputs=39):
         super(MyLSTM, self).__init__()
         self.ninput = n_inputs
         self.device = device
@@ -30,10 +30,10 @@ class MyLSTM(nn.Module):
         # optimizer = optim.Adam(chain(*[lstm.parameters(),fc1.parameters(),fc2.parameters()]), lr=0.0001)
         # optimizer = optim.SGD(lstm.parameters(), lr=0.001,momentum=True)
 
-    def ResetHiddenState(self):   #metodo che resetta l'hidden ???
-        self.hidden = (torch.rand(1,1,3).to(self.device),torch.rand(1,1,3).to(self.device))
+    def ResetHiddenState(self):   # metodo che resetta l'hidden ???
+        self.hidden = (torch.rand(1, 1, 3).to(self.device), torch.rand(1, 3).to(self.device))
 
-    def forward(self, input):  #dato l'input fornisce l'output
+    def forward(self, input):  # dato l'input fornisce l'output
         input = self.fc1(input)
         input = self.ReLU(input)
         input = self.fcm(input)
@@ -76,7 +76,7 @@ class MyLSTMCell(nn.Module):
     def ResetHiddenState(self):   # metodo che resetta l'hidden ???
         self.hidden = (torch.rand(self.nlayers, 1, 3), torch.rand(self.nlayers, 1, 3))
 
-    def forward(self, input):  #dato l'input fornisce l'output
+    def forward(self, input):  # dato l'input fornisce l'output
 
         out, self.hidden, self.cell = self.lstm(input.view(1, 1, self.ninput), self.hidden, self.cell)
         return out
@@ -94,16 +94,17 @@ class MyLSTMgru(nn.Module):
     def ResetHiddenState(self):   # metodo che resetta l'hidden ???
         self.hidden = (torch.rand(self.nlayers, 1, 3), torch.rand(self.nlayers, 1, 3))
 
-    def forward(self, input):  #dato l'input fornisce l'output
+    def forward(self, input):  # dato l'input fornisce l'output
 
         out, self.hidden = self.lstm(input.view(1, 1, self.ninput), self.hidden)
         return out
 
-#model = torch.nn.Sequential(lstm,
+# model = torch.nn.Sequential(lstm,
 #                            torch.nn.ReLU(),
 #                            torch.nn.Linear(3,3)) # fully connected layer
 # import pdb; pdb.set_trace() # mette i breakpoints
 # torch.cat(inputs).view(len(inputs), 1, 9) #se come terzo valore metto -1 funziona con tutto perchè chiedo a lui di farlo arbitrariamente
+
 
 class MyLSTM_integrated(nn.Module):
     def __init__(self,device = torch.device("cpu"),n_inputs=39):
@@ -120,10 +121,10 @@ class MyLSTM_integrated(nn.Module):
         # optimizer = optim.Adam(chain(*[lstm.parameters(),fc1.parameters(),fc2.parameters()]), lr=0.0001)
         # optimizer = optim.SGD(lstm.parameters(), lr=0.001,momentum=True)
 
-    def ResetHiddenState(self):   #metodo che resetta l'hidden ???
-        self.hidden = (torch.rand(1,1,3).to(self.device),torch.rand(1,1,3).to(self.device))
+    def ResetHiddenState(self):   # metodo che resetta l'hidden ???
+        self.hidden = (torch.rand(1, 1, 3).to(self.device), torch.rand(1, 1, 3).to(self.device))
 
-    def forward(self, input):  #dato l'input fornisce l'output
+    def forward(self, input):  # dato l'input fornisce l'output
         input = self.fc1(input)
         input = self.ReLU(input)
         input = self.fcm(input)
@@ -132,6 +133,7 @@ class MyLSTM_integrated(nn.Module):
         out = self.ReLU(out)    # out = F.relu(out)
         out = self.fc2(out)
         return out
+
 
 class YOLO_LSTM(nn.Module):
     def __init__(self, input_dim, output_size, hidden_dim, n_layers, drop_prob=0.0):
@@ -143,16 +145,15 @@ class YOLO_LSTM(nn.Module):
         self.lstm = nn.LSTM(input_dim, hidden_dim, n_layers, dropout=drop_prob, batch_first=True)
         self.dropout = nn.Dropout(drop_prob)
         self.fc = nn.Linear(hidden_dim, output_size)
-        
-    def forward(self, x,hidden_state = None):
+
+    def forward(self, x, hidden_state=None):
         if hidden_state is None:
-            lstm_out, hidden = self.lstm(x) # dimensione di x; batch_size * seq * element
+            lstm_out, hidden_state = self.lstm(x) # dimensione di x; batch_size * seq * element
             lstm_out = lstm_out.contiguous().view(-1, self.hidden_dim)
             out = self.fc(lstm_out)
             return out
         else:
-            lstm_out, hidden = self.lstm(x,hidden) # dimensione di x; batch_size * seq * element
+            lstm_out, hidden_state = self.lstm(x, hidden_state) # dimensione di x; batch_size * seq * element
             lstm_out = lstm_out.contiguous().view(-1, self.hidden_dim)
             out = self.fc(lstm_out)
-            return out, hidden
-
+            return out, past_meas
