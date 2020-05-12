88693ba257f37df3d152e14823709c90fa21483e
fabidicia <62762235+fabidicia@users.noreply.github.com>
2020-05-12 22:09:32 +0200

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -3,6 +3,7 @@ from torch.utils.data import Dataset
 import torch
 import csv
 from math import sin,cos,atan
+import scipy.io
 
 class IMUdata(Dataset):
 
@@ -103,3 +104,30 @@ class SimpleDataset(Dataset):
         input_list = torch.stack(input_list)
         gt_list = torch.stack(gt_list)
         return input_list, gt_list
+class datasetMatlabIMU(Dataset):
+
+    def __init__(self, path="/mnt/c/Users/fabia/OneDrive/Desktop/Deep_Learning/dataMatrix.mat"):
+        self.path = path
+        with open(self.path) as imudata:
+            data = scipy.io.loadmat(path)
+        self.orient = data['orient']
+        self.gyr_s = data['gyr_s']
+        self.mag_s = data['mag_s']
+        self.acc_s = data['acc_s']
+        self.epoch_acc = data['epoch_acc']
+        self.len = self.orient.shape[0]
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, n):   # METODO
+        orient = tuple(elem for elem in self.orient[n])
+        gyr = tuple(elem for elem in self.gyr_s[n])
+        acc_v = tuple(elem for elem in self.acc_s[n])
+        mag = tuple(elem for elem in self.mag_s[n])
+                
+
+        time = self.epoch_acc[n, 0] # BOOH non so cosa ci sta qua dentro!
+        gt_rot = None
+        gt_transl = None
+        return time, orient, acc_v, gyr, mag, gt_rot, gt_transl #decreta chiusura del metodo, deve essere l'ultima riga
